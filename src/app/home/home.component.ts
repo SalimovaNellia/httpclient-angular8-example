90658414a541a7ac83cae2ac98a26a5b9019bac2
Nellia Salimova <salimova.nellia@gmail.com>
2019-11-08 23:46:26 +0200

    unsubscribing from HttpClient observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { DataService } from '../services/data.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   products = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor( private dataService: DataService ) { }
 
   ngOnInit() {
-    this.dataService.sendGetRequest().subscribe((data: any[]) => {
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
       console.log(data);
       this.products = data;
     })
+  }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
 }
